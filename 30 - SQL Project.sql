9980939b1ac9eca6b7bce764ed7c0346cddc61e2
prasadpatil1123 <122286888+prasadpatil1123@users.noreply.github.com>
2026-03-03 15:25:19 +0530

    'afternoon'

--- 30 - SQL Project.sql
+++ 30 - SQL Project.sql
@@ -121,17 +121,46 @@ having count(order_id) > 2;
 
 -- 4) Find the most frequently ordered book:
 
+select o.book_id, b.title, count(o.order_id) as order_count
+from orders o join books b on o.book_id = b.book_id
+group by o.book_id, b.title
+order by order_count desc limit 1;
+
 -- 5) Show the top 3 most expensive books of 'Fantasy' Genre :
 
+select * from books
+where genre = 'Fantasy'
+order by price desc limit 3;
 
 -- 6) Retrieve the total quantity of books sold by each author:
 
+select b.author, sum(o.quantity) as total_qty
+from orders o join books b on o.book_id = b.book_id
+group by b.author;
 
 -- 7) List the cities where customers who spent over $30 are located:
+
+select * from orders;
+select distinct c.city, o.total_amount
+from customers c 
+join orders o
+on c.customer_id = o.customer_id
+where o.total_amount > 30;
 
 
 -- 8) Find the customer who spent the most on orders:
 
-
+select c.customer_id, c.name, sum(o.total_amount) as total_spent
+from customers c join orders  o
+on c.customer_id = o.customer_id
+group by c.customer_id, c.name
+order by total_spent desc limit 1;
 
 --9) Calculate the stock remaining after fulfilling all orders:
+
+select b.book_id, b.title, b.stock, coalesce(sum(o.quantity),0) as order_quantity, 
+b.stock - coalesce(sum(o.quantity),0) as results
+from books b left join orders o
+on b.book_id = o.book_id
+group by b.book_id
+order by b.book_id;
